b225f586e86dbeb3919faca84b2ac8338de482e1
Miguel Fernandes <miguel.fernandes.idesign@gmail.com>
2024-08-26 15:08:31 +0100

    1.3

--- assets/js/recap.js
+++ assets/js/recap.js
@@ -1,5 +1,4 @@
 function enableSubmitBtn() {
-    // Identifica o botão correto com base no contexto da página
     const contactsButton = document.getElementById('submitBtnContacts');
 
     // Se o botão existe na página, habilite-o e aplique o estilo apropriado
@@ -8,14 +7,13 @@ function enableSubmitBtn() {
         contactsButton.style.color = "#fff";
         contactsButton.style.cursor = "pointer";
         console.log('Botão habilitado:', contactsButton.id);
+
+        // Adiciona um evento de clique ao botão para mudar o texto e mostrar o spinner
+        contactsButton.addEventListener('click', function() {
+            contactsButton.innerHTML = "<i class='fas fa-spinner fa-spin'></i> A enviar"; // Altera o texto e adiciona o spinner
+            contactsButton.disabled = true; // Desativa o botão enquanto o formulário está sendo enviado
+        });
     } else {
         console.error('Botão não encontrado');
     }
 }
-
- // Adiciona um evento de clique ao botão para mudar o texto e mostrar o spinner
- document.getElementById('submitBtnContacts').addEventListener('click', function(event) {
-    var submitButton = this;
-    submitButton.innerHTML = "<i class='fas fa-spinner fa-spin'></i> A enviar"; // Altera o texto e adiciona o spinner
-    submitButton.disabled = true; // Desativa o botão enquanto o formulário está sendo enviado
-});
